db12f68faee59c61bc01441261c011c8261be61a
Kyle <kjbenk@gmail.com>
2014-05-01 08:38:20 +0200

    Clean up uninstall.php

--- uninstall.php
+++ uninstall.php
@@ -16,25 +16,6 @@
 		// Delete blog option
 		
 		delete_option($version_name);
-		
-		/*
-// Delete post meta data
-		
-		$posts = get_posts(array('posts_per_page' => -1));
-		
-		foreach ($posts as $post) {
-			$post_meta = get_post_meta($post->ID);
-			delete_post_meta($post->ID, $post_meta_data_name);
-		} 
-		
-		// Delete user meta data
-		
-		$users = get_users();
-		
-		foreach ($users as $user) {
-			delete_user_meta($user->ID, $user_meta_data_name);
-		}
-*/
 	} 
 	
 	else {
@@ -42,39 +23,5 @@
 		// Delete site option
 	
 		delete_site_option($version_name);
-		
-		/*
-// Used to delete each option from each blog
-		
-	    $blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
-	
-	    foreach ( $blog_ids as $blog_id ) 
-	    {
-	        switch_to_blog( $blog_id );
-	        
-	        // Delete blog option
-		
-			delete_option($blog_option_name);
-			
-			// Delete post meta data
-			
-			$posts = get_posts(array('posts_per_page' => -1));
-			
-			foreach ($posts as $post) {
-				$post_meta = get_post_meta($post->ID);
-				delete_post_meta($post->ID, $post_meta_data_name);
-			} 
-			
-			// Delete user meta data
-			
-			$users = get_users();
-			
-			foreach ($users as $user) {
-				delete_user_meta($user->ID, $user_meta_data_name);
-			}
-	        
-	        restore_current_blog();
-	    }
-*/
 	}
 ?>
