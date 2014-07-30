6385b4f69b7a11faeb932c3037e485dc4c272190
Kyle <kjbenk@gmail.com>
2014-07-30 17:24:06 -0400

    Remove Line Endings

--- readme.txt
+++ readme.txt
@@ -1,6 +1,6 @@
 === WPsite Post Status Notifications ===
 Contributors: wpsitenet, kjbenk, charliepatel
-Donate link: 
+Donate link:
 Tags: post status, status, status notification, post notification, email notification, post status notification, notifier, contributors, submit for review, pending status, pending posts, published posts, contributions, post message, article submissions, admin notifications, editor notifications, contributor notification, post email, editorial workflow, edit flow
 Requires at least: 3.8
 Tested up to: 3.9
@@ -15,26 +15,26 @@ The Post Status Notifications plugin by WPsite.net provides an easy way to ensur
 The Post Status Notifications plugin by WPsite.net provides an easy way to ensure Administrators are notified of their Contributors posts submitted for review. Additionally, when an administrator publishes the post, the original author will also be notified via email so that they can see their post live on the site. These email notifications ensure posts are not sitting idle waiting for reviewers who otherwise would have to check WordPress admin manually for posts in the pending review status.
 
 = Features =
-Get a better editorial workflow by ensuring communication occurs related to articles submitted for review and subsequently published by administrators. 
+Get a better editorial workflow by ensuring communication occurs related to articles submitted for review and subsequently published by administrators.
 
 * Email notification to Admin when a post is submitted for review
 * Email notification to Contributor when a post is published on the site
 
-This is great plugin for every site that accepts user submitted content, has multiple authors, or site owners running content-heavy websites. Enhance your editorial workflow with proper communication to the relevant parties. 
+This is great plugin for every site that accepts user submitted content, has multiple authors, or site owners running content-heavy websites. Enhance your editorial workflow with proper communication to the relevant parties.
 
 For more details, visit the http://www.wpsite.net for more information.
 
 == Installation ==
 
-You can install the Gabfire Widget Pack from your WordPress Dashboard or manually via FTP. 
+You can install the Gabfire Widget Pack from your WordPress Dashboard or manually via FTP.
 
 = From WordPress Dashboard =
 Similar to all other plugins, follow these instructions:
 
-* Navigate to 'Plugins -> Add New' from your WordPress dashboard. 
-* Search for `Post Status Notifications` and install it. 
+* Navigate to 'Plugins -> Add New' from your WordPress dashboard.
+* Search for `Post Status Notifications` and install it.
 * Activate the plugin from Plugins menu.
-* Configure the plugin's options from Post Status Notifications tab in your WP Admin. 
+* Configure the plugin's options from Post Status Notifications tab in your WP Admin.
 
 = Manual Installation =
 
@@ -42,7 +42,7 @@ Similar to all other plugins, follow these instructions:
 * Unzip the file
 * Upload the `wpsite-post-status-notifications` folder to your `/wp-content/plugins` directory (do not rename the folder)
 * Activate the plugin from Plugins menu.
-* Configure the plugin's options from Post Status Notifications tab in your WP Admin. 
+* Configure the plugin's options from Post Status Notifications tab in your WP Admin.
 
 == Screenshots ==
 
--- wpsite_post_status_notification.php
+++ wpsite_post_status_notification.php
@@ -9,8 +9,8 @@ Author URI: http://wpsite.net
 License: GPL2
 */
 
-/** 
- * Global Definitions 
+/**
+ * Global Definitions
  */
 
 /* Plugin Name */
@@ -27,31 +27,31 @@ if (!defined('WPSITE_POST_STATUS_NOTIFICATION_PLUGIN_DIR'))
 
 if (!defined('WPSITE_POST_STATUS_NOTIFICATION_PLUGIN_URL'))
     define('WPSITE_POST_STATUS_NOTIFICATION_PLUGIN_URL', WP_PLUGIN_URL . '/' . WPSITE_POST_STATUS_NOTIFICATION_PLUGIN_NAME);
-  
+
 /* Plugin verison */
 
 if (!defined('WPSITE_POST_STATUS_NOTIFICATION_VERSION_NUM'))
     define('WPSITE_POST_STATUS_NOTIFICATION_VERSION_NUM', '1.1.0');
 
 
-/** 
- * Activatation / Deactivation 
- */  
+/**
+ * Activatation / Deactivation
+ */
 
 register_activation_hook( __FILE__, array('WPSitePostStatusNotifications', 'register_activation'));
 
-/** 
- * Hooks / Filter 
+/**
+ * Hooks / Filter
  */
- 
+
 add_action('transition_post_status', array('WPSitePostStatusNotifications', 'wpsite_send_email'), 10, 3 );
 add_action('init', array('WPSitePostStatusNotifications', 'load_textdoamin'));
 add_action('admin_menu', array('WPSitePostStatusNotifications', 'wpsite_admin_menu'));
 
-$plugin = plugin_basename(__FILE__); 
+$plugin = plugin_basename(__FILE__);
 add_filter("plugin_action_links_$plugin", array('WPSitePostStatusNotifications', 'wpsite_post_status_notification_settings_link'));
 
-/** 
+/**
  *  WPSitePostStatusNotifications main class
  *
  * @since 1.0.0
@@ -61,27 +61,27 @@ add_filter("plugin_action_links_$plugin", array('WPSitePostStatusNotifications',
 class WPSitePostStatusNotifications {
 
 	/* Properties */
-	
+
 	private static $version_setting_name = 'wpsite_post_status_notification_verison';
-	
+
 	private static $text_domain = 'wpsite-post-status-notification';
-	
+
 	private static $settings_page = 'wpsite-post-status-notification-admin-settings';
-	
+
 	private static $web_page = 'http://www.wpsite.net/plugin/post-status-notifications';
-	
+
 	/* Share Link */
-	
+
 	private static $facebook_share_link = 'https://www.facebook.com/sharer/sharer.php?u=';
-	
+
 	private static $twitter_share_link = 'https://twitter.com/intent/tweet?url=';
-	
+
 	private static $google_share_link = 'https://plus.google.com/share?url=';
-	
+
 	private static $linkedin_share_link = 'https://www.linkedin.com/shareArticle?url=';
-	
+
 	/* Default Settings */
-	
+
 	private static $default = array(
 		'notify'		=> 'author',
 		'post_types'	=> array('post'),
@@ -105,50 +105,50 @@ class WPSitePostStatusNotifications {
 	);
 
 	/**
-	 * Load the text domain 
-	 * 
+	 * Load the text domain
+	 *
 	 * @since 1.0.0
 	 */
 	static function load_textdoamin() {
 		load_plugin_textdomain(self::$text_domain, false, WPSITE_POST_STATUS_NOTIFICATION_PLUGIN_DIR . '/languages');
 	}
-	
+
 	/**
-	 * Hooks to 'register_activation_hook' 
-	 * 
+	 * Hooks to 'register_activation_hook'
+	 *
 	 * @since 1.0.0
 	 */
 	static function register_activation() {
-	
+
 		/* Check if multisite, if so then save as site option */
-		
+
 		if (is_multisite()) {
 			add_site_option(self::$version_setting_name, WPSITE_POST_STATUS_NOTIFICATION_VERSION_NUM);
 		} else {
 			add_option(self::$version_setting_name, WPSITE_POST_STATUS_NOTIFICATION_VERSION_NUM);
 		}
 	}
-	
+
 	/**
-	 * Hooks to 'plugin_action_links_' filter 
-	 * 
+	 * Hooks to 'plugin_action_links_' filter
+	 *
 	 * @since 1.0.0
 	 */
-	static function wpsite_post_status_notification_settings_link($links) { 
-		$settings_link = '<a href="tools.php?page=' . self::$settings_page . '">Settings</a>'; 
-		array_unshift($links, $settings_link); 
-		return $links; 
+	static function wpsite_post_status_notification_settings_link($links) {
+		$settings_link = '<a href="options-general.php?page=' . self::$settings_page . '">Settings</a>';
+		array_unshift($links, $settings_link);
+		return $links;
 	}
-	
+
 	/**
-	 * Hooks to 'admin_menu' 
-	 * 
+	 * Hooks to 'admin_menu'
+	 *
 	 * @since 1.0.0
 	 */
 	static function wpsite_admin_menu() {
-	
+
 		 /* Cast the first sub menu to the settings menu */
-	    
+
 	    $settings_page_load = add_submenu_page(
 	    	'options-general.php', 												// parent slug
 	    	__('WPsite Post Status Notifications', self::$text_domain), 						// Page title
@@ -159,48 +159,48 @@ class WPSitePostStatusNotifications {
 	    );
 	    add_action("admin_print_scripts-$settings_page_load", array('WPSitePostStatusNotifications', 'inline_scripts_admin'));
 	}
-	
+
 	/**
-	 * Hooks to 'admin_print_scripts-$page' 
-	 * 
+	 * Hooks to 'admin_print_scripts-$page'
+	 *
 	 * @since 1.0.0
 	 */
 	static function inline_scripts_admin() {
 		wp_enqueue_style('wpsite_post_status_notifications_admin_css', WPSITE_POST_STATUS_NOTIFICATION_PLUGIN_URL . '/css/wpsite_post_status_notifications_admin.css');
 	}
-	
+
 	/**
-	 * Callback to info page 
-	 * 
+	 * Callback to info page
+	 *
 	 * @since 1.0.0
 	 */
 	static function wpsite_admin_menu_info_callback() {
-	
+
 		/* Get all post types that are public */
-										
-		$post_types = get_post_types(array('public' => true)); 
-	
+
+		$post_types = get_post_types(array('public' => true));
+
 		$settings = get_option('wpsite_post_status_notifications_settings');
-			
+
 		/* Default values */
-		
+
 		if ($settings === false) {
 			$settings = self::$default;
 		}
-		
+
 		/* Save data nd check nonce */
-		
+
 		if (isset($_POST['submit']) && check_admin_referer('wpsite_post_status_notifications_admin_settings')) {
-		
+
 			/* Determine Post Types */
-			
+
 			$post_types_array = array();
-			
+
 			foreach ($post_types as $post_type) {
-				if (isset($_POST['wpsite_post_status_notifications_settings_post_types_' . $post_type]) && $_POST['wpsite_post_status_notifications_settings_post_types_' . $post_type]) 
+				if (isset($_POST['wpsite_post_status_notifications_settings_post_types_' . $post_type]) && $_POST['wpsite_post_status_notifications_settings_post_types_' . $post_type])
 					$post_types_array[] = $post_type;
 			}
-			
+
 			$settings = array(
 				'notify'		=> $_POST['wpsite_post_status_notifications_settings_notify_users'],
 				'post_types'	=> $post_types_array,
@@ -222,49 +222,49 @@ class WPSitePostStatusNotifications {
 					)
 				)
 			);
-			
+
 			/* 'active'	=> isset($_POST['wpsite_follow_us_settings_twitter_active']) && $_POST['wpsite_follow_us_settings_twitter_active'] ? true : false, */
-			
+
 			update_option('wpsite_post_status_notifications_settings', $settings);
 		}
-		
+
 		wp_enqueue_script('jquery');
 		wp_enqueue_script('jquery-ui-tabs');
-		
+
 		require('admin/dashboard.php');
 	}
-	
+
 	/**
-	 * Email all admins when post status changes to pending 
-	 * 
+	 * Email all admins when post status changes to pending
+	 *
 	 * @since 1.0.0
 	 */
 	static function wpsite_send_email( $new_status, $old_status, $post ) {
-	
+
 		$settings = get_option('wpsite_post_status_notifications_settings');
-			
+
 		/* Default values */
-		
+
 		if ($settings === false) {
 			$settings = self::$default;
 		}
-		
+
 		// Set all headers
-		
+
 		$headers = array();
-		
+
 		if (isset($settings['message']['from_email']) && $settings['message']['from_email'] != '') {
 			$headers[] = "From: " . $settings['message']['from_email'] . "\r\n";
 		}
-		
+
 		if (isset($settings['message']['cc_email']) && $settings['message']['cc_email'] != '') {
 			$headers[] = "Cc: " . $settings['message']['cc_email'] . "\r\n";
 		}
-		
+
 		if (isset($settings['message']['bcc_email']) && $settings['message']['bcc_email'] != '') {
 			$headers[] = "Bcc: " . $settings['message']['bcc_email'] . "\r\n";
 		}
-		
+
 		if (isset($settings['message']['share_links'])) {
 			$check = false;
 			foreach ($settings['message']['share_links'] as $link) {
@@ -273,189 +273,189 @@ class WPSitePostStatusNotifications {
 				}
 			}
 		}
-		
+
 		$url = get_permalink($post->ID);
 		$share_links = '';
-		
+
 		if (isset($share_links_check) && $share_links_check) {
 			$share_links = "\r\n\r\nShare Links\r\n";
-			
+
 			if ($settings['message']['share_links']['twitter']) {
 				$share_links .= "Twitter: " . esc_url(self::$twitter_share_link . $url) . "\r\n";
 			}
-			
+
 			if ($settings['message']['share_links']['facebook']) {
 				$share_links .= "Facebook: " . esc_url(self::$facebook_share_link . $url) . "\r\n";
 			}
-			
+
 			if ($settings['message']['share_links']['google']) {
 				$share_links .= "Google+: " . esc_url(self::$google_share_link . $url) . "\r\n";
 			}
-			
+
 			if ($settings['message']['share_links']['linkedin']) {
 				$share_links .= "LinkedIn: " . esc_url(self::$linkedin_share_link . $url) . "\r\n";
 			}
 		}
-		
+
 		$wpsite_info = "\r\n\r\nThis was sent by WPsite Post Status Notifications." .  "\r\n" .  "wpsite.net";
 		$just_published_contributor = '"' . $post->post_title . '"' . " was just published!.  Check it out, and thanks for the hard work.\r\n";
 	    $just_published = '"' . $post->post_title . '"' . " was just published!.\r\n";
-	
+
 		// Notifiy Admins that Contributor has writen a post
-		
+
 	    if (in_array($post->post_type, $settings['post_types']) && $new_status == 'pending' && user_can($post->post_author, 'edit_posts') && !user_can($post->post_author, 'publish_posts')) {
-	    	
+
 	    	$url = get_permalink($post->ID);
 	    	$edit_link = get_edit_post_link($post->ID, '');
 			$preview_link = get_permalink($post->ID) . '&preview=true';
 	    	$username = get_userdata($post->post_author);
-	    	
+
 	    	// Custom Subject and Message
-	    	
+
 	    	if (isset($settings['message']['subject_pending']) && $settings['message']['subject_pending'] != '') {
 				$subject = $settings['message']['subject_pending'];
 			} else {
 				$subject = 'Please moderate: "' . $post->post_title . '"';
 			}
-			
+
 			if (isset($settings['message']['content_pending']) && $settings['message']['content_pending'] != '') {
 				$message = $settings['message']['content_pending'];
 			} else {
 				$message = 'A new ' . $post->post_type . ': "' . $post->post_title . '" from contributor: ' . $username->user_login . ' is now pending.';
 			}
-	    	
+
 	    	$message .= "\r\n\r\n";
 	    	$message .= "Author: $username->user_login\r\n";
 	    	$message .= "Title of " . $post->post_type . ": $post->post_title";
-	    	
+
 	    	$message .= "\r\n\r\n";
 	    	$message .= "Edit the " . $post->post_type . ": $edit_link\r\n";
 	    	$message .= "Preview it: $preview_link";
-	    	
+
 	    	$message .= $wpsite_info;
-	    	
+
 			$users = get_users(array(
 				'role'	=> 'administrator'
 			));
-			
+
 			foreach ($users as $user) {
 				$result = wp_mail($user->user_email, $subject, $message, $headers);
 			}
 	    }
-	    
+
 	    // Notifiy Contributor or All Admins or All Users that a post was published
-	    
+
 	    if (in_array($post->post_type, $settings['post_types']) && $new_status == 'publish') {
-	    
+
 	    	// Notify Contributor that their post was published
-	    
+
 	    	if (isset($settings['notify']) && $settings['notify'] == 'author' && $old_status == 'pending' && user_can($post->post_author, 'edit_posts') && !user_can($post->post_author, 'publish_posts')) {
 				$username = get_userdata($post->post_author);
-				
+
 				// Custom Subject and Message
-				
+
 				if (isset($settings['message']['subject_published_contributor']) && $settings['message']['subject_published_contributor'] != '') {
 					$subject = $settings['message']['subject_published_contributor'];
 				} else {
 					$subject = "Published " . $post->post_type . ":" . " " . $post->post_title;
 				}
-				
+
 				if (isset($settings['message']['content_published_contributor']) && $settings['message']['content_published_contributor'] != '') {
 					$message = $settings['message']['content_published_contributor'];
 				} else {
 					$message = $just_published_contributor . $url;
 				}
-				
+
 				$message .= $share_links . $wpsite_info;
-				
+
 				$result = wp_mail($username->user_email, $subject, $message, $headers);
 			}
-		   
+
 			// Notify All Admins or All Users
-			
+
 			if (isset($settings['notify']) && $settings['notify'] != 'author') {
-			
+
 				// Notify All Admins
-			
+
 				if ($settings['notify'] == 'admins' && ($old_status == 'pending' || $old_status != $new_status)) {
-				
+
 					// Custom Subject and Message
-	    	
+
 			    	if (isset($settings['message']['subject_published']) && $settings['message']['subject_published'] != '') {
 						$subject = $settings['message']['subject_published'];
 					} else {
 						$subject = "Published " . $post->post_type . ":" . " " . $post->post_title;
 					}
-					
+
 					if (isset($settings['message']['content_published']) && $settings['message']['content_published'] != '') {
 						$message = $settings['message']['content_published'];
 					} else {
 						$message = $just_published . $url;
 					}
-					
+
 			    	$message .= $share_links . $wpsite_info;
-		    	
-		    	
+
+
 					$users = get_users(array(
 						'role'		=> 'administrator'
 					));
-					
+
 					foreach ($users as $user) {
 						$result = wp_mail($user->user_email, $subject, $message, $headers);
 					}
 				}
-				
+
 				// Notify All Users
-				
+
 				if ($settings['notify'] == 'users' && ($old_status == 'pending' || $old_status != $new_status)) {
-				
+
 					$exclude_array = array();
-				
+
 					if ($old_status == 'pending' && user_can($post->post_author, 'edit_posts') && !user_can($post->post_author, 'publish_posts')) {
-						
+
 						$username = get_userdata($post->post_author);
-						
+
 						// Custom Subject and Message
-	    	
+
 				    	if (isset($settings['message']['subject_published_contributor']) && $settings['message']['subject_published_contributor'] != '') {
 							$subject = $settings['message']['subject_published_contributor'];
 						} else {
 							$subject = "Published " . $post->post_type . ":" . " " . $post->post_title;
 						}
-						
+
 						if (isset($settings['message']['content_published_contributor']) && $settings['message']['content_published_contributor'] != '') {
 							$message = $settings['message']['content_published_contributor'];
 						} else {
 							$message = $just_published_contributor . $url;
 						}
-				    	
+
 				    	$message .= $share_links . $wpsite_info;
-						
+
 						$result = wp_mail($username->user_email, $subject, $message, $headers);
-						
+
 						$exclude_array[] = $post->post_author;
 					}
-					
+
 					// Custom Subject and Message
-	    	
+
 			    	if (isset($settings['message']['subject_published']) && $settings['message']['subject_published'] != '') {
 						$subject = $settings['message']['subject_published'];
 					} else {
 						$subject = "Published " . $post->post_type . ":" . " " . $post->post_title;
 					}
-					
+
 					if (isset($settings['message']['content_published']) && $settings['message']['content_published'] != '') {
 						$message = $settings['message']['content_published'];
 					} else {
 						$message = $just_published . $url;
 					}
-					
+
 			    	$message .= $share_links . $wpsite_info;
-				
+
 					$users = get_users(array(
 						'exclude'	=> $exclude_array
 					));
-					
+
 					foreach ($users as $user) {
 						$result = wp_mail($user->user_email, $subject, $message, $headers);
 					}
