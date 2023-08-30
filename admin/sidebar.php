676fad69d5ca4807ec56364db3b4a1288d414b93
Alex Lion <learnwithalex@gmail.com>
2023-08-29 21:42:28 -0700

    Improve I18N Issues (Based on 3.2.0)
    - The current text domain used in the codes is `wpsite-post-status-notification` but the correct text domain is `wpsite-post-status-notifications`. Plugin's text domain must be equal to its owned slug `wpsite-post-status-notifications`, and **should not** be a typo (missing **s**). This means even if this plugin's GlotPress locale project is done, this plugin still display localized UI strings incorrectly.
    - This PR fixed this issue.

--- admin/sidebar.php
+++ admin/sidebar.php
@@ -2,7 +2,7 @@
 
 	<div class="panel panel-default">
 		<div class="panel-heading">
-			<h3 class="panel-title"><?php esc_html_e( 'Signup now to get notified of plugin updates, awesome themes, and more. Over 10,000+ already have:', 'wpsite-post-status-notification' ) ?></h3>
+			<h3 class="panel-title"><?php esc_html_e( 'Signup now to get notified of plugin updates, awesome themes, and more. Over 10,000+ already have:', 'wpsite-post-status-notifications' ) ?></h3>
 		</div>
 		<div class="panel-body">
 			<!-- Begin MailChimp Signup Form -->
@@ -20,14 +20,14 @@
 			<form action="//wpsite.us5.list-manage.com/subscribe/post?u=82c2341134bbdc37714642adb&amp;id=642b18616e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate style="padding-left: 0;">
 				<div id="mc_embed_signup_scroll">
 					<div style="margin-bottom: 20px;">
-						<input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="<?php esc_html_e( 'Email Address', 'wpsite-post-status-notification' ) ?>">
+						<input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL" placeholder="<?php esc_html_e( 'Email Address', 'wpsite-post-status-notifications' ) ?>">
 					</div>
 					<div id="mce-responses" class="clear">
 						<div class="response" id="mce-error-response" style="display:none"></div>
 						<div class="response" id="mce-success-response" style="display:none"></div>
 					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 					<div style="position: absolute; left: -5000px;"><input type="text" name="b_82c2341134bbdc37714642adb_642b18616e" tabindex="-1" value=""></div>
-					<div><input style="width:100%;" type="submit" value="Subscribe" name="subscribe" class="btn btn-default"></div>
+					<div><input style="width:100%;" type="submit" value="<?php esc_attr_e( 'Subscribe', 'wpsite-post-status-notifications' ) ?>" name="subscribe" class="btn btn-default"></div>
 				</div>
 			</form>
 			</div>
@@ -38,18 +38,18 @@
 
 	<div class="panel panel-default">
 		<div class="panel-heading">
-			<h3 class="panel-title"><?php esc_html_e( 'Must-Read Articles', 'wpsite-post-status-notification' ) ?></h3>
+			<h3 class="panel-title"><?php esc_html_e( 'Must-Read Articles', 'wpsite-post-status-notifications' ) ?></h3>
 		</div>
 		<div class="panel-body">
 			<div class="wpsite_feed">
-				<script src="http://feeds.feedburner.com/99robots?format=sigpro" type="text/javascript" ></script><noscript><p><?php esc_html_e( 'Subscribe to 99 Robots Feed:', 'wpsite-post-status-notification' ) ?> <a href="http://feeds.feedburner.com/99robots"></a><br/><?php esc_html_e( 'Powered by FeedBurner', 'wpsite-post-status-notification' ) ?></p> </noscript>
+				<script src="http://feeds.feedburner.com/99robots?format=sigpro" type="text/javascript" ></script><noscript><p><?php esc_html_e( 'Subscribe to 99 Robots Feed:', 'wpsite-post-status-notifications' ) ?> <a href="http://feeds.feedburner.com/99robots"></a><br/><?php esc_html_e( 'Powered by FeedBurner', 'wpsite-post-status-notifications' ) ?></p> </noscript>
 			</div>
 		</div>
 	</div>
 
 	<div class="panel panel-default">
 		<div class="panel-heading">
-			<h3 class="panel-title"><?php esc_html_e( 'Need a Plugin or Theme Developed?', 'wpsite-post-status-notification' ) ?></h3>
+			<h3 class="panel-title"><?php esc_html_e( 'Need a Plugin or Theme Developed?', 'wpsite-post-status-notifications' ) ?></h3>
 		</div>
 		<div class="panel-body">
 			<a class="nnr-sidebar-image-link" href="https://draftpress.com/contact?utm_medium=sidebar&utm_campaign=plugin-request-banner" target="_blank">
